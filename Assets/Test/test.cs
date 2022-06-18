1dd5e6405c67da64a5ccee91588093796755b98e
yuan0717 <4a9k0017@stust.edu.tw>
2022-06-18 21:43:09 +0800

    damnnnnn

--- Assets/Test/test.cs
+++ Assets/Test/test.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    public GameObject ball;
+    public Transform spawnpoint;
     public void Buttontest()
     {
         Debug.Log(1);
     }
+    public void genball()
+    {
+        Instantiate(ball, spawnpoint.position, Quaternion.identity);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            genball();
+        }
+    }
 }
+
